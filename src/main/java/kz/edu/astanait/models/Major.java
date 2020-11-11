14d93bd5c7f93b438bf6fdb168acdb9406be0581
sazizbeks <sazizbeks@gmail.com>
2020-11-11 23:01:24 +0600

    Model Major & updated ERD

---
+++ src/main/java/kz/edu/astanait/models/Major.java
@@ -0,0 +1,35 @@
+package kz.edu.astanait.models;
+
+public class Major {
+    private String id;
+    private String full_name;
+
+    public Major(String id, String full_name) {
+        this.id = id;
+        this.full_name = full_name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    @Override
+    public String toString() {
+        return "Major{" +
+                "id='" + id + '\'' +
+                ", full_name='" + full_name + '\'' +
+                '}';
+    }
+}

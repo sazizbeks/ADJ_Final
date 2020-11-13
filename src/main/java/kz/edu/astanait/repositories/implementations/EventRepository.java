db4508a1c3e313e52b9353cfb4c51dcbb3d8c3de
sazizbeks <sazizbeks@gmail.com>
2020-11-13 19:31:32 +0600

    EventRepository.add()

--- src/main/java/kz/edu/astanait/repositories/implementations/EventRepository.java
+++ src/main/java/kz/edu/astanait/repositories/implementations/EventRepository.java
@@ -1,26 +1,35 @@
 package kz.edu.astanait.repositories.implementations;
 
 import kz.edu.astanait.databases.Postgres;
+import kz.edu.astanait.exceptions.NotFoundException;
 import kz.edu.astanait.models.Event;
+import kz.edu.astanait.models.Student;
 import kz.edu.astanait.repositories.interfaces.IEventRepository;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.LinkedList;
 import java.util.List;
 
 public class EventRepository implements IEventRepository {
 
     @Override
-    public Event queryOne(String sql) {
-        return null;
-    }
-
-    @Override
-    public List<Event> getAll() {
-        String sql = "SELECT * FROM events";
-        return findSeveral(sql);
+    public Event queryOne(String sql) throws NotFoundException {
+        try {
+            Statement statement = Postgres.getConnection().createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            if (rs.next()) {
+                return new Event.Builder()
+                        .setId(rs.getInt("event_id"))
+                        .setName(rs.getString("event_name"))
+                        .setStart_date(rs.getDate("event_start_date"))
+                        .setEnd_date(rs.getDate("event_end_date"))
+                        .setCreator_id(rs.getInt("event_creator_id"))
+                        .build();
+            }
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+        throw new NotFoundException("Not Found");
     }
 
     @Override
@@ -47,8 +56,24 @@ public class EventRepository implements IEventRepository {
     }
 
     @Override
-    public void add(Event entity) {
+    public List<Event> getAll() {
+        String sql = "SELECT * FROM events";
+        return findSeveral(sql);
+    }
 
+    @Override
+    public void add(Event entity) {
+        String sql = "INSERT INTO events(EVENT_NAME, EVENT_START_DATE, EVENT_END_DATE, EVENT_CREATOR_ID) VALUES (?,?,?,?)";
+        try {
+            PreparedStatement ps = Postgres.getConnection().prepareStatement(sql);
+            ps.setString(1, entity.getName());
+            ps.setDate(2, (Date) entity.getStart_date());
+            ps.setDate(3, (Date) entity.getEnd_date());
+            ps.setInt(4, entity.getCreator_id());
+            ps.execute();
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
     }
 
     @Override

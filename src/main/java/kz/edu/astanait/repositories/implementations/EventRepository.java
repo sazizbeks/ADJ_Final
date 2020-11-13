3eb64dce9123871412e6457b55f6e37e7f304742
sazizbeks <sazizbeks@gmail.com>
2020-11-13 22:30:47 +0600

    EventRepository

--- src/main/java/kz/edu/astanait/repositories/implementations/EventRepository.java
+++ src/main/java/kz/edu/astanait/repositories/implementations/EventRepository.java
@@ -3,10 +3,10 @@ package kz.edu.astanait.repositories.implementations;
 import kz.edu.astanait.databases.Postgres;
 import kz.edu.astanait.exceptions.NotFoundException;
 import kz.edu.astanait.models.Event;
-import kz.edu.astanait.models.Student;
 import kz.edu.astanait.repositories.interfaces.IEventRepository;
 
 import java.sql.*;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -61,14 +61,27 @@ public class EventRepository implements IEventRepository {
         return findSeveral(sql);
     }
 
+
+    /**
+     * Example of usage:
+     * IEventRepository rep = new EventRepository();
+     * rep.add(new Event.Builder().
+     * setName("Testing Event add")
+     * .setStart_date(new Date())
+     * .setEnd_date(new Date(2020-1900, Calendar.DECEMBER,14))
+     * .setCreator_id(1)
+     * .build());
+     *
+     * @param entity - object of Event
+     */
     @Override
     public void add(Event entity) {
         String sql = "INSERT INTO events(EVENT_NAME, EVENT_START_DATE, EVENT_END_DATE, EVENT_CREATOR_ID) VALUES (?,?,?,?)";
         try {
             PreparedStatement ps = Postgres.getConnection().prepareStatement(sql);
             ps.setString(1, entity.getName());
-            ps.setDate(2, (Date) entity.getStart_date());
-            ps.setDate(3, (Date) entity.getEnd_date());
+            ps.setTimestamp(2, dateToTimestamp(entity.getStart_date()));
+            ps.setTimestamp(3, dateToTimestamp(entity.getEnd_date()));
             ps.setInt(4, entity.getCreator_id());
             ps.execute();
         } catch (SQLException throwable) {
@@ -76,13 +89,64 @@ public class EventRepository implements IEventRepository {
         }
     }
 
+    /**
+     * Example of usage:
+     * IEventRepository rep = new EventRepository();
+     *         rep.update(new Event.Builder()
+     *                 .setId(1)
+     *                 .setName("Testing Event")
+     *                 .setStart_date(new Date())
+     *                 .setEnd_date(new Date(2020-1900, Calendar.DECEMBER,14, 15,16,17))
+     *                 .setCreator_id(2)
+     *                 .build());
+     *
+     * @param entity - object of Event
+     */
     @Override
     public void update(Event entity) {
+        StringBuilder sql = new StringBuilder("UPDATE EVENTS SET");
+        if (entity.getName() != null) sql.append(" event_name=?,");
+        if (entity.getStart_date() != null) sql.append(" event_start_date=?,");
+        if (entity.getEnd_date() != null) sql.append(" event_end_date=?,");
+        if (entity.getCreator_id() != null) sql.append(" event_creator_id=?,");
+        sql.deleteCharAt(sql.length() - 1);
+        sql.append(" WHERE event_id=?");
 
+        try {
+            PreparedStatement ps = Postgres.getConnection().prepareStatement(sql.toString());
+            int i = 1;
+            if (entity.getName() != null) ps.setString(i++, entity.getName());
+            if (entity.getStart_date() != null) ps.setTimestamp(i++, dateToTimestamp(entity.getStart_date()));
+            if (entity.getEnd_date() != null) ps.setTimestamp(i++, dateToTimestamp(entity.getEnd_date()));
+            if (entity.getCreator_id() != null) ps.setInt(i++, entity.getCreator_id());
+            ps.setInt(i, entity.getId());
+            ps.execute();
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
     }
 
+    /**
+     * Example of usage:
+     * IEventRepository rep = new EventRepository();
+     *         rep.delete(new Event.Builder()
+     *                 .setId(1)
+     *                 .build());
+     *
+     * @param entity - object of Event
+     */
     @Override
     public void delete(Event entity) {
+        String sql = "DELETE FROM events WHERE event_id=" + entity.getId();
+        try {
+            Postgres.getConnection().createStatement().execute(sql);
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+    }
 
+    // Method to convert java.util.Date to java.sql.Timestamp
+    private Timestamp dateToTimestamp(Date date) {
+        return new Timestamp(date.getTime());
     }
 }

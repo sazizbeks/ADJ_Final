945c2b75d497651e348546d02e35c78ecbe6006f
kamekato <kamekato@mail.ru>
2020-11-15 14:19:09 +0600

    Group Service, News Service.

---
+++ src/main/java/kz/edu/astanait/api/rest/GroupService.java
@@ -0,0 +1,58 @@
+package kz.edu.astanait.api.rest;
+import kz.edu.astanait.models.Group;
+import kz.edu.astanait.repositories.implementations.GroupRepository;
+import kz.edu.astanait.repositories.interfaces.IGroupRepository;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+
+@Path("/group")
+public class GroupService {
+    private final IGroupRepository groupRepository = new GroupRepository();
+
+    @GET
+    @Path("/getAll")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAll() {
+        List<Group> groups = groupRepository.getAll();
+        return Response.ok(groups).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response add(Group group) {
+        try {
+            groupRepository.add(group);
+        } catch (BadRequestException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+        return Response.status(Response.Status.CREATED).build();
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response update(Group old, Group newG) {
+        try {
+            groupRepository.update(old, newG);
+        } catch (BadRequestException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+        return Response.ok("Group successfully modified.").build();
+    }
+
+    @POST
+    @Path("/delete")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response delete(Group group) {
+        try {
+            groupRepository.delete(group);
+        } catch (BadRequestException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+        return Response.ok("Group successfully deleted.").build();
+    }
+
+}
---
+++ src/main/java/kz/edu/astanait/api/rest/NewsService.java
@@ -0,0 +1,55 @@
+package kz.edu.astanait.api.rest;
+import kz.edu.astanait.models.News;
+import kz.edu.astanait.repositories.implementations.NewsRepository;
+import kz.edu.astanait.repositories.interfaces.INewsRepository;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+public class NewsService {
+    private final INewsRepository newsRepository = new NewsRepository();
+
+    @GET
+    @Path("/getAll")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAll() {
+        List<News> news = newsRepository.getAll();
+        return Response.ok(news).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response add(News news) {
+        try {
+            newsRepository.add(news);
+        } catch (BadRequestException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+        return Response.status(Response.Status.CREATED).build();
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response update(News news) {
+        try {
+            newsRepository.update(news);
+        } catch (BadRequestException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+        return Response.ok("News successfully modified.").build();
+    }
+
+    @POST
+    @Path("/delete")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response delete(News news) {
+        try {
+            newsRepository.delete(news);
+        } catch (BadRequestException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+        return Response.ok("News successfully deleted.").build();
+    }
+}
